6274c602692a48490eaf9dd39be2aa612493660e
amdadulhaquerubel <ahrubel77@gmail.com>
2018-09-13 22:07:13 +0600

    project added,

---
+++ ConsoleApplication4/ConsoleApplication4/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Person
+    {
+        public Person(string firstName, string middleName, string lastName)
+        {
+            
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+
+        }
+
+        public Person(string firstName,string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName; 
+        }
+
+        public Person()
+        {
+            
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string MiddleName
+        {
+            get { return middleName; }
+            set { middleName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string GetFullName()
+        {
+            string fullname = FirstName + " " + MiddleName + " " + LastName;
+            return fullname;
+
+        }
+    }
+}
---
+++ ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FirstName = "Liakot";
+            person1.MiddleName = "Ali";
+            person1.LastName = "Fokir";
+            string fullName = person1.GetFullName();
+            Person person2=new Person();
+            Console.WriteLine(fullName);
+            Console.ReadKey();
+        }
+    }
+}
